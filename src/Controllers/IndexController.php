9d3f27e3c4d18db57688806835191d8861a49534
Tommy <meier@rhinos-media.de>
2016-05-09 17:11:29 +0200

    probably testing

--- src/Controllers/IndexController.php
+++ src/Controllers/IndexController.php
@@ -5,7 +5,13 @@ class IndexController extends \Slim\Mvc\Controller
 
     public function indexAction()
     {
-        
+        $fs = new \Beast\Cloud\Filesystem\Filesystem('');
+
+        $fs->makeDir('dev/php/beast_cloud');
+
+        $fs->cd('dev/php/beast_cloud')->write('index.html', 'Hello World');
+
+        var_dump($fs->ls());
     }
 
 }
